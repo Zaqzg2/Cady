3ec6a09a949f075ca8da22b9f8de4c29e417633a
Zaqzg2 <zaqzg2@gmail.com>
2026-09-18 08:16:22 +0300

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,8 +90,8 @@ dependencies {
     implementation(libs.kotlinx.coroutines.play.services)
 
     implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.auth.ktx)
-    implementation(libs.firebase.firestore.ktx)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
 
     implementation(libs.androidx.datastore.preferences)
 
